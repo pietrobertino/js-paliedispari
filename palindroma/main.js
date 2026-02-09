53c3b2b2e5136e66178db776fa3c5438872354fe
pietrobertino <pietrobertino20@gmail.com>
2026-02-09 17:42:15 +0100

    first commit

---
+++ palindroma/main.js
@@ -0,0 +1,32 @@
+// Chiedo all'utente una parola
+const user_word = prompt("Inserire una parola");
+
+//Creo la funzione per verificare se una parola è palindroma
+function isPalindrome (word) {
+    //creo una variabile per rovesciare la parola di partenza
+    let reverse_word = "";
+
+    //inverto la parola
+    for (i = word.length-1; i>=0; i--) {
+        let char = word[i];
+        reverse_word += char;
+    }
+
+    //SE la parola di partenza e quella invertita coincidono
+    if (word === reverse_word) {
+        //la parola è un palindromo
+        return true;
+    }
+    //ALTRIMENTI
+    else {
+        //la parola non è un palindromo
+        return false;
+    }
+}
+
+if (isPalindrome(user_word)) {
+    console.log("La parola inserita è un palindromo");
+} else { 
+    console.log("La parola inserita non è un palindromo");
+    
+}
